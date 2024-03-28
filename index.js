24a64063f09fb3e8df6335d193efa77017ba0348
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-03-28 16:00:46 +0300

    completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 14;
+let num4 = 5;
+let num5 = 20;
 
 // Write your code here
+const multiply = num1 * num2 // multiply
+
+const mod = num3 % num4; // mod
+
+const max = Math.max(num2, num3, num4, num5); // max
+
+const random = Math.floor(Math.random() * 10) + 1; // + 1 ensures that number generated isnt 0
+
+
